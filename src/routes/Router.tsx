b7a770291107fa453c05f06eece2833e59c67dbd
hansolbangul <ruaenddl981028@naver.com>
2022-11-01 17:31:02 +0900

    feat : 스타일 추가

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChurMap } from '../pages/ChurMap';
+import { Mobile } from '../styled';
 
 function Router() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ChurMap />}></Route>
-      </Routes>
+      <Mobile>
+        <Routes>
+          <Route path="/" element={<ChurMap />}></Route>
+        </Routes>
+      </Mobile>
     </BrowserRouter>
   );
 }
