9ec04d0233e4ad6219f2ef8d2ba84b4cf1f5d3a5
hansolbangul <ruaenddl981028@naver.com>
2022-11-02 19:37:32 +0900

    feat : 스타일 추가

--- front/src/styled.ts
+++ front/src/styled.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
+import { HiPlusSm, HiX } from "react-icons/hi";
 export const maxWidth = window.innerWidth
 
 
 export const Mobile = styled.div`
   width: 100vw;
-  height: 100vh;
 
   @media screen and (max-width: 500px) {
     width: ${maxWidth < 500 ? maxWidth : 500}px;
@@ -20,4 +20,22 @@ export const Flex = styled.div<{ justify?: string, margin?: string, align?: stri
   ${props => props.justify && `justify-content: ${props.justify};`}
   ${props => props.margin && `margin: ${props.margin};`}
   ${props => props.align && `align-items: ${props.align};`}
+`
+
+export const Img = styled.img`
+
+`
+
+export const Plus = styled(HiPlusSm)`
+  color: #fff;
+  font-size: 32px;
+`
+
+export const Cancel = styled(HiX)`
+  color: #fff;
+  font-size: 20px;
+`
+
+export const Tag = styled.div`
+  
 `
