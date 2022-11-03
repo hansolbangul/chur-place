b09ed157b5603185d683d8d358f8a7f5232a5037
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2022-11-03 20:45:48 +0900

    fix : 환경변수 추가

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
 
-var testHost = 'http://localhost:3010';
+var testHost = process.env.REACT_APP_BACK_BASE_URL;
 
 export const serverHost = testHost;
 
 export const Axios = axios.create({
   baseURL: `${serverHost}`,
-  timeout: 3000,
+  timeout: 180000,
   withCredentials: true,
+  headers: {
+    Authorization: `Bearer ${''}` // 토큰 넣어야 함
+  }
 });
