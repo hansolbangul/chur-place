bc79860a93045dabab70b4cc43c15da7ab597aa9
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2022-11-03 20:44:36 +0900

    feat : swiper css 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import Router from './routes/Router';
+import 'swiper/swiper.min.css';
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
