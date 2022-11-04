a33f07973c16010868090fd91334ed10a97654ca
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2022-11-04 10:51:04 +0900

    feat : svg 추가

---
+++ front/src/custom.d.ts
@@ -0,0 +1,18 @@
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
+declare module '*.jpeg' {
+  const path: string;
+  export default path;
+}
+
+declare module "*.jpg" {
+  const path: string;
+  export default path;
+}
+
+declare module "*.png" {
+  const path: string;
+  export default path;
+}
