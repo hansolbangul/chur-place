a43ba07d19cfc98750c3c087b141419f939ef7ee
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2022-11-04 10:51:20 +0900

    feat : 인터페이스 추가

--- front/src/ts/interface.ts
+++ front/src/ts/interface.ts
@@ -17,3 +17,7 @@ export interface IGetCatInfo {
 }
 
 export interface ILatLon { x: number; y: number; _lat: number; _lng: number }
+
+export interface IType { type_id: number; name: string }
+
+export interface ITag { tag_id: number; name: string }
