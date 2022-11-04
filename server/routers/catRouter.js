76f4cf807141eb1a6f00388069a197026fe94174
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2022-11-04 10:51:35 +0900

    feat : 고양이 정보 api 추가

---
+++ server/routers/catRouter.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import authUtil from '../modules/auth.js';
+
+import { response, not_found, bad_response, not_response } from '../modules/responseMSG.js';
+
+const catRouter = express.Router();
+import _pool from '../modules/database.js';
+const pool = _pool();
+
+let query = '';
+
+catRouter.get('/type', async (req, res, next) => {
+  console.log('user');
+
+  query =
+    'select * from type';
+  const [data] = await pool.query(query);
+
+  if (data.length > 0) {
+    res.status(201).json(await response(data));
+  } else {
+    res.json(await not_found());
+  }
+});
+
+catRouter.get('/tag', async (req, res, next) => {
+  console.log('user');
+
+  query =
+    'select * from tag';
+  const [data] = await pool.query(query);
+
+  if (data.length > 0) {
+    res.status(201).json(await response(data));
+  } else {
+    res.json(await not_found());
+  }
+});
+
+
+catRouter.get('/:id', async (req, res, next) => {
+  const cat_id = req.params.id;
+
+  let comment_query =
+    'select c.id, c.cat_id, c.comment, c.create_date, m.member_name from comment as c left join member as m on m.member_id = c.member_id where c.cat_id = ?';
+
+  let tag_query = 'select name from cat_to_tag left join tag on tag.tag_id = cat_to_tag.tag_id where cat_id = ?'
+
+  const [[comment], [tag]] = await Promise.all([
+    pool.query(comment_query, [cat_id]),
+    pool.query(tag_query, [cat_id])
+  ])
+
+  res.status(201).json(await response({ comment: comment, tag: tag }));
+});
+
+export default catRouter;
