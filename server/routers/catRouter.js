e9606800482f1d1cee9a5ed5fbfce832f7edcaf3
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2022-11-07 13:59:06 +0900

    feat : 고양이 등록

--- server/routers/catRouter.js
+++ server/routers/catRouter.js
@@ -9,6 +9,42 @@ const pool = _pool();
 
 let query = '';
 
+catRouter.post('/', async (req, res, next) => {
+  const body = req.body;
+
+  query = 'insert into `cat` (`age`, `lat`, `lon`, `type`, `member_id`, `gender`) values (?, ?, ?, ?, ?, ?)'
+  const [rows] = await pool.query(query, [0, body.lat, body.lon, body.type, 0, 0]);
+
+  body.tag.forEach(async (item) => {
+    query = 'insert into cat_to_tag (cat_id, tag_id) values (?, ?)';
+    await pool.query(query, [rows.insertId, item]);
+  })
+
+  query = 'select name from cat left join type on cat.type = type.type_id where cat.cat_id = ?'
+
+  const [name] = await pool.query(query, [rows.insertId]);
+
+  if (rows.insertId) {
+    res.status(201).json(await response({ ...name[0], cat_id: rows.insertId }));
+  } else {
+    res.json(await not_found());
+  }
+});
+
+catRouter.post('/comment/:id', async (req, res, next) => {
+  const body = req.body;
+  const cat_id = req.params.id;
+
+  query = 'insert into comment (cat_id, member_id, comment) values (?, ?, ?)'
+  const [rows] = await pool.query(query, [cat_id, 1, body.comment]);
+
+  if (rows.insertId) {
+    res.status(201).json(await response());
+  } else {
+    res.json(await not_found());
+  }
+});
+
 catRouter.get('/type', async (req, res, next) => {
   console.log('user');
 
@@ -37,14 +73,13 @@ catRouter.get('/tag', async (req, res, next) => {
   }
 });
 
-
 catRouter.get('/:id', async (req, res, next) => {
   const cat_id = req.params.id;
 
   let comment_query =
     'select c.id, c.cat_id, c.comment, c.create_date, m.member_name from comment as c left join member as m on m.member_id = c.member_id where c.cat_id = ?';
 
-  let tag_query = 'select name from cat_to_tag left join tag on tag.tag_id = cat_to_tag.tag_id where cat_id = ?'
+  let tag_query = 'select name from cat_to_tag left join tag on tag.tag_id = cat_to_tag.tag_id where cat_to_tag.cat_id = ?'
 
   const [[comment], [tag]] = await Promise.all([
     pool.query(comment_query, [cat_id]),
