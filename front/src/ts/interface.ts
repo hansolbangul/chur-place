42846e366b6963a58571201d3e51d52abece3c37
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2022-11-07 13:58:54 +0900

    feat : 인터페이스 추가

--- front/src/ts/interface.ts
+++ front/src/ts/interface.ts
@@ -21,3 +21,8 @@ export interface ILatLon { x: number; y: number; _lat: number; _lng: number }
 export interface IType { type_id: number; name: string }
 
 export interface ITag { tag_id: number; name: string }
+
+export interface ISelect {
+  type: number;
+  tag: number[]
+}
