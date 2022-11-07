9e224e75bf93742b1d8556264bd85b52a3287a90
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2022-11-07 13:58:27 +0900

    feat : 노티 빼기

--- front/src/routes/Router.tsx
+++ front/src/routes/Router.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChurMap } from '../pages/ChurMap';
 import { Mobile } from '../styled';
+import { maxWidth } from '../styled';
+import { ToastContainer } from 'react-toastify';
+import styled from 'styled-components';
 
 function Router() {
 
@@ -11,9 +14,21 @@ function Router() {
         <Routes>
           <Route path="/nmap" element={<ChurMap />}></Route>
         </Routes>
+        <ToastAlert position="top-center" />
       </Mobile>
     </BrowserRouter>
   );
 }
 
 export default Router;
+
+const ToastAlert = styled(ToastContainer)`
+  
+
+  @media screen and (max-width: 500px) {
+    width: ${maxWidth < 500 && 300}px;
+    top: 20px;
+    left: 50%;
+    transform: translate(-50%, 0)
+  }
+`
