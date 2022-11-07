631ee5710682c241bd911ad695ce4fda32d18097
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2022-11-07 13:58:45 +0900

    feat : 노티 뺴기

--- front/src/ts/export.ts
+++ front/src/ts/export.ts
@@ -1,1 +1,17 @@
+import { toast } from "react-toastify";
+
 export const defaultImage = require(`../img/cat-face.png`)
+
+export const warning_notify = (message: string) => {
+  toast.warning(message, {
+    autoClose: 1000,
+    hideProgressBar: true,
+  });
+};
+
+export const success_notify = (message: string) => {
+  toast.success(message, {
+    autoClose: 1000,
+    hideProgressBar: true,
+  });
+};
