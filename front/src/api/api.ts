ff0295250caa7cf7b1011b35dcd40d41f3806473
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2022-11-09 09:58:55 +0900

    fix : 개발, 배포 환경 설정

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 // env 파일에서 dev, pro 로 설정 가능
-// var testHost = process.env.REACT_APP_MODE === 'dev' ? process.env.REACT_APP_BACK_BASE_URL : process.env.REACT_APP_BACK_PRO_URL;
+var testHost = process.env.REACT_APP_MODE === 'dev' ? process.env.REACT_APP_BACK_BASE_URL : process.env.REACT_APP_BACK_PRO_URL;
 
-var testHost = process.env.REACT_APP_BACK_BASE_URL;
+// var testHost = process.env.REACT_APP_BACK_BASE_URL;
 
 export const serverHost = testHost;
 
